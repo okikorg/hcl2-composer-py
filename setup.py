b30a3cc04b5daede507345f1730d8bb1ee6a3be9
akashsokik <akash@okik.co.uk>
2024-05-10 16:41:06 +0100

    setup.py added

--- setup.py
+++ setup.py
@@ -1,1 +1,21 @@
 # Setup
+from setuptools import setup, find_packages
+
+__version__ = "0.0.1"
+
+# Read requirements.txt and store contents in a list
+with open("./requirements.txt") as f:
+    required = f.read().splitlines()
+
+setup(
+    name="okik",
+    version=__version__,
+    packages=find_packages(),
+    author="Okik",
+    author_email="hello@okik.co.uk",
+    description="HCL2 code generator for Terraform",
+    install_requires=[required],
+    classifiers=[
+        "Programming Language :: Python :: 3.11",
+    ],
+)
