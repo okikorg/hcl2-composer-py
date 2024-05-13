e5396c1582a8a2c166cbc93ba7be6cbe9eafaa21
akashsokik <akash@okik.co.uk>
2024-05-13 14:26:52 +0100

    pyproject.toml added

--- setup.py
+++ setup.py
@@ -1,20 +1,24 @@
 from setuptools import setup, find_packages
 
-__version__ = "0.0.9"
-
-# Read requirements.txt and store contents in a list
-with open("./requirements.txt") as f:
-    required = f.read().splitlines()
-
 setup(
     name="py2hcl2",
-    version=__version__,
-    packages=find_packages(),
+    version="0.0.9",
+    description="A package for generating HCL blocks in Python",
     author="Okik",
     author_email="hello@okik.co.uk",
-    description="HCL2 code generator for Terraform",
-    install_requires=required,
-    classifiers=[
-        "Programming Language :: Python :: 3.11",
+    url="https://github.com/okikorg/py2hcl2",  # Update with your actual GitHub URL
+    packages=find_packages(),
+    install_requires=[
+        "pydantic>=1.8.2",  # Specify required dependencies
+        "rich>=10.0.0"
     ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
 )
+[build-system]
+requires = ["setuptools", "wheel"]
+build-backend = "setuptools.build_meta"
